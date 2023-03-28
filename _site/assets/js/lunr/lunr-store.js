d44c53522545e0da7b615fa29d4f74cbc4bc23ca
237-series <237.silly.mistake@gmail.com>
2023-03-28 11:22:06 +0900

    sky diary privacy update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": ["Swift","pdf"],
         "url": "/swift/swift-2020-incnu/",
         "teaser": null
+      },{
+        "title": "Sky Diary Privacy",
+        "excerpt":"‘SkyDiary’ 앱 개인정보 취급 방침 ‘SkyDiary’는 개인정보보호법에 따라 이용자들의 개인정보 보호 및 권익을 보호하고자 다음과 같은 처리방침을 두고 있습니다. 당사는 개인정보처리방침을 개정하는 경우 앱 화면 및 웹사이트 공지사항을 통하여 공지할 것입니다. ​ 1. 개인정보의 처리 목적 : 당사에서 개인정보를 별도로 저장하거나 이용하지 않습니다. ​ 2. 개인정보 파일 현황 당사는 별도의...","categories": [],
+        "tags": [],
+        "url": "/sky-diary-privacy/",
+        "teaser": null
       }]
