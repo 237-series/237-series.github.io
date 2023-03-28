43f80ae89fab052523be3fa17779e6f2d3f1f8c5
237-series <237.silly.mistake@gmail.com>
2023-03-28 10:58:51 +0900

    update theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jeky!!!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/first-post/",
+        "teaser": null
+      },{
+        "title": "Swift 2020 특강자료",
+        "excerpt":"스위프트 특강 자료     첫번째 날: 자료 다운받기   ","categories": ["Swift"],
+        "tags": ["Swift","pdf"],
+        "url": "/swift/swift-2020-incnu/",
+        "teaser": null
+      }]
